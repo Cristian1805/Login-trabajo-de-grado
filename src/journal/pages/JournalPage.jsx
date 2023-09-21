ad04d124c30935e2d8318b93dd19178123ccc919
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-09-21 13:29:36 -0500

    first commit

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+import { MailLockOutlined } from '@mui/icons-material';
+
+
+export const JournalPage = () => {
+  return (
+    <>
+    <Typography variant='h1'> JournalPage </Typography> 
+    <MailLockOutlined />
+
+    </>
+  )
+}
